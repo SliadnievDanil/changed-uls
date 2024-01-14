452ebd268d37ec5b2b0a892eed191559cc9d86dd
Danil Sliadniev <danil@Linux.myguest.virtualbox.org>
2024-01-14 19:42:50 +0200

    t01

--- src/my_custom_create_arr_from_list.c
+++ src/my_custom_create_arr_from_list.c
@@ -1,13 +1,13 @@
 #include "uls.h"
 
-CustomElement **my_custom_create_arr_from_list(t_list *objects_info, int *counter) {
+t_element **my_custom_create_arr_from_list(t_list *objects_info, int *counter) {
     *counter = my_custom_list_size(objects_info);
 
     if (*counter == 0) {
         return NULL;
     }
 
-    CustomElement **array = malloc(sizeof(CustomElement *) * (*counter));
+    t_element **array = malloc(sizeof(t_element *) * (*counter));
 
     for (int i = 0; i < *counter; i++) {
         array[i] = objects_info->data;
