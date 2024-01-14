5e34f53f0bee59bd5baaea478f2a012c926dfe40
Danil Sliadniev <danil@Linux.myguest.virtualbox.org>
2024-01-14 19:37:01 +0200

    t01

--- src/my_custom_change_unprintable.c
+++ src/my_custom_change_unprintable.c
@@ -1,7 +1,7 @@
 #include "uls.h"
 
 char *my_custom_change_unprintable(const char *name) {
-    char *cpy = my_custom_strdup(name);
+    char *cpy = mx_strdup(name);
 
     for (int i = 0; cpy[i] != '\0'; i++) {
         if (cpy[i] >= 0 && cpy[i] <= 31) {
